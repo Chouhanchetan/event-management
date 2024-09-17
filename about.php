de9735d150e7d91c40e7f4e7b8ff0fc084c6d928
Chouhanchetan <chouhanchetan114@gmail.com>
2024-09-17 19:20:21 +0530

    Update about.php

--- about.php
+++ about.php
@@ -18,5 +18,26 @@
     <p>Adornment was created with the notion that all events should be unique and thoughtful. Our team has worked in the Portland area for over 15 years in weddings and special events. We’ve done everything from an orchard country wedding of 20 people, to managing high-touch corporate galas and multi-day events. Our foundation of events is built on providing a full-service and freshly creative team to our special clients.</p>
     <p>Truth – we love what we do, and take pride in creating experiences for our clients!</p>
     </div>
+    <center><hr class="hr"></center>
+
+    <div class="hii"><h1>The Team</h1></div>
+
+    <div class="hello1">
+<div class="list"><img src="" alt=""></div>
+<div class="list"><img src="" alt=""></div>
+<div class="list"><img src="" alt=""></div>
+<div class="list"><img src="" alt=""></div>
+</div>
+<div class="hello1">
+<div class="list"><img src="" alt=""></div>
+<div class="list"><img src="" alt=""></div>
+<div class="list"><img src="" alt=""></div>
+<div class="list"><img src="" alt=""></div>
+</div>
+<center><hr class="hr"></center>
+   <?php
+   include('footer.html');
+   ?>
+
 </body>
 </html>
