d5b6e60056ea7a1ecaa2d8291d9f0b47d90b7c75
Chouhanchetan <chouhanchetan114@gmail.com>
2024-09-22 16:31:30 +0530

    Update kindwords.php

--- kindwords.php
+++ kindwords.php
@@ -5,17 +5,99 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>kindwords</title>
     <link rel="stylesheet" href="kindwords.css">
-
-
+    <style>
+        body{
+            background:rgb(240, 234, 223);
+        }
+        h1{
+            font-family: "Dancing Script", cursive;
+    font-optical-sizing: auto;
+    font-weight: 800;
+    font-style: normal;
+    color: rgb(223, 168, 52);
+    font-size: xxx-large;
+    align-items: center;
+        }
+        .hr{
+            width: 30%;
+            opacity: 0.5;
+        }
+        .container{
+            width: 100%;
+    justify-content: space-around;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+        }
+        .container1{
+            width: 80%;
+    height: 500px;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+        }
+        .container2{
+            height: 419px;
+            width:663px;
+            background: rgb(245, 221, 169);
+            padding-left: 45px;
+            padding-top: 20px;
+            border-radius:10%
+        }
+        .container3{
+            height: 419px;
+            width:663px;
+            background: rgb(245, 221, 169);
+            padding-left: 45px;
+            padding-top: 20px;
+            border-radius:10%
+            
+        }
+        .im{
+            width: 500px;
+            height: 500px;
+        }
+    </style>
 </head>
 <body>
     <?php
     include('nav.html');
-    ?>
-
-    <?php
-    include('footer.html');
-    ?>
-
+    ?> <br>
+     <div class="container">
+    <h1>Kind Words</h1>
+    </div>
+    <center><hr class="hr"></center><br><br><br>
+    <div class="container1">
+    <div class="container2">
+        <h2>PRODUCT LAUNCH</h2>
+        <p>Many companies we deal with have a need to promote new products both internally to their own organization and externally to their clients, buyers, dealer or press. Professionally we elevate your presentation with simple set and staging. With proper light effects, sound, data backup and professional technical advice. We use dynamic lighting effects, gobos, scanners etc to give a real feel of movement, change, and exciting environment. It is associated with warm and soft or alive and kicking, sound. We can certainly come up with ideas for that as well.</p>
+       </div>
+    <img class="im" src="" alt="">
+</div>
+<br><br><br>
+<div class="container1">
+    <img class="im" src="" alt="">
+    <div class="container3">
+        <h2>CONFERENCE & SEMINARS</h2>
+        <p>We ensure success of any conferences or seminars by well crafted event planning & Management also reviewing that’s our strength. Our ability to get into the minutest of the details enables us to secure & handle every aspect right from hospitality to the most advanced technical requirements of any conference or seminar. The ideal customer experience in an event is a comprehensive, yet flexible framework for achieving customer loyalty by learning and understanding your needs.</p>
+</div>
+</div>
+<br><br><br>
+<div class="container1">
+    <div class="container2">
+        <h2>PRODUCT LAUNCH</h2>
+        <p>Many companies we deal with have a need to promote new products both internally to their own organization and externally to their clients, buyers, dealer or press. Professionally we elevate your presentation with simple set and staging. With proper light effects, sound, data backup and professional technical advice. We use dynamic lighting effects, gobos, scanners etc to give a real feel of movement, change, and exciting environment. It is associated with warm and soft or alive and kicking, sound. We can certainly come up with ideas for that as well.</p>
+       </div>
+    <img class="im" src="" alt="">
+</div>
+<br><br><br>
+<div class="container1">
+    <img class="im" src="" alt="">
+    <div class="container3">
+        <h2>CONFERENCE & SEMINARS</h2>
+        <p>We ensure success of any conferences or seminars by well crafted event planning & Management also reviewing that’s our strength. Our ability to get into the minutest of the details enables us to secure & handle every aspect right from hospitality to the most advanced technical requirements of any conference or seminar. The ideal customer experience in an event is a comprehensive, yet flexible framework for achieving customer loyalty by learning and understanding your needs.</p>
+</div>
+</div>
 </body>
 </html>
