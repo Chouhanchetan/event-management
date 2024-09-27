5fc00717909300d96e48fbb96eceaf4f9a1255e6
Chouhanchetan <chouhanchetan114@gmail.com>
2024-09-27 19:32:42 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -170,45 +170,45 @@ fieldset {
     ?>
 
 <div class="container">  
-  <form id="contact" action="" method="post">
+  <form id="contact" action="query.php" method="post">
     <h3>Contact </h3>
     <fieldset>
         <b>First & Last Name</b><br>
-      <input type="text" tabindex="1" required autofocus>
+      <input type="text" tabindex="1" name="fullname" required autofocus>
     </fieldset>
     <fieldset>
         <b>Email Address</b><br>
-      <input  type="email" tabindex="2" required>
+      <input  type="email" tabindex="2" name="mail" required>
     </fieldset>
     <fieldset>
         <b>Phone Number</b><br>
-      <input type="tel" tabindex="3" required>
+      <input type="tel" tabindex="3" required name="contact">
     </fieldset>
     <fieldset>
         <b>Wedding or Special Event Date</b><br>
-      <input type="date" tabindex="1" required autofocus>
+      <input type="date" tabindex="1" required autofocus name="date">
     </fieldset>
     <fieldset>
         <b>Event Location (or considering)</b><br>
-        <input type="text" tabindex="1" required autofocus>
+        <input type="text" tabindex="1" required autofocus name="location">
         </fieldset>
         <fieldset>
         <b>How many guests are you anticipating?</b><br>
-        <input type="text" tabindex="1" required autofocus>
+        <input type="text" tabindex="1" required autofocus name="guests">
         </fieldset>
-        <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+        <input type="radio" id="vehicle1" name="planning" value="FPDW">
   <label for="vehicle1"><b>Full Planning + Design - Wedding</b></label><br>
-  <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+  <input type="radio" id="vehicle2" name="planning" value="EP">
   <label for="vehicle2"><b>Engagement Party</b></label><br>
-  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <input type="radio" id="vehicle3" name="planning" value="CE">
   <label for="vehicle3"><b>Corporate event</b></label><br>
-  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <input type="radio" id="vehicle3" name="planning" value="BP">
   <label for="vehicle3"><b>Birthday Party</b></label><br>
-  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <input type="radio" id="vehicle3" name="planning" value="NS">
   <label for="vehicle3"><b>Not sure, let's talk</b></label>
   <fieldset><br>
         <b>More Details the Better - Tell us more of how we can help, your vision, and any details you know now</b><br>
-        <textarea id="message" class="form-control rounded border-white mb-3 form-text-area" rows="5" cols="30" placeholder="Message" required></textarea>
+        <textarea id="message" class="form-control rounded border-white mb-3 form-text-area" rows="5" cols="30" placeholder="Message" required name="details"></textarea>
     </fieldset>
     <fieldset>
       <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
