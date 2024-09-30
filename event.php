f36393e995adc1bf1e030646eb3056ec4efaa75f
Chouhanchetan <chouhanchetan114@gmail.com>
2024-09-30 22:41:23 +0530

    Update event.php

--- event.php
+++ event.php
@@ -79,14 +79,14 @@
 <body>
     <?php
     include('nav.html');
-    ?><br>
+    ?><br> <br><br><br><br>
          <center><hr class="hr"></center>
 
      <div class="hello1" data-aos="fade-in" data-aos-duration="1000"     data-aos-once="true">
-<div class="list"><img src="" alt=""></div>
-<div class="list"><img src="" alt=""></div>
-<div class="list"><img src="" alt=""></div>
-<div class="list"><img src="" alt=""></div>
+<div class="list"><img src="A.jpg" alt=""></div>
+<div class="list"><img src="C.jpg" alt=""></div>
+<div class="list"><img src="B.jpg" alt=""></div>
+<div class="list"><img src="D.jpg" alt=""></div>
 </div><br><br><br><br>
 
 <div class="container">
@@ -96,13 +96,13 @@
           <p>Every single client is unique and the design of an event should speak to that. We believe in curating our designs by honoring our clients stories, personalities and inspirations. Whether you are looking to host your event in the Pacific NW or define your style with a unique destination, we support you with the intention your journey deserves.</p>
       <br>
     <div class="color2">
-       <a href="destination.php"><p><b>>DESTINATION WEDDINGS</b></p></a>
        <a href="Engagement.php"><p><b>>ENGAGEMENT PARTY</b></p></a>
+       <a href="destination.php"><p><b>>DESTINATION WEDDINGS</b></p></a>
        <a href="corporate.php"><p><b>>CORPORATE EVENTS</b></p></a>
-       <a href="birthday.php"><p><b>>BIRTHDAY PARTY</b></p></a>       
+       <a href="Events.php"><p><b>>EVENTS </b></p></a>       
     </div>
 </div>
-<img class="im" data-aos="fade-left" data-aos-duration="1000"     data-aos-once="ture" src="" alt="">
+<img class="im" data-aos="fade-left" data-aos-duration="1000"     data-aos-once="ture" src="E1.jpg" alt="">
 </div><br><br><br>
 
      <center><hr class="hr"></center>
