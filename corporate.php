7f5efd8d2122ad1c3e150e542245c9824b8a0356
Chouhanchetan <chouhanchetan114@gmail.com>
2024-09-30 22:40:30 +0530

    Update corporate.php

--- corporate.php
+++ corporate.php
@@ -13,7 +13,7 @@
     font-optical-sizing: auto;
     font-weight: 800;
     font-style: normal;
-    color: rgb(223, 168, 52);
+    color:#564332 ;
     font-size: xxx-large;
     align-items: center;
         }
@@ -87,7 +87,7 @@
     <?php
     include('nav.html');
     ?>
-    <br>
+    <br> <br><br><br><br>
     <div class="container">
     <h1>Corporate Events</h1>
     </div>
@@ -98,11 +98,11 @@
         <h2>PRODUCT LAUNCH</h2>
         <p>Many companies we deal with have a need to promote new products both internally to their own organization and externally to their clients, buyers, dealer or press. Professionally we elevate your presentation with simple set and staging. With proper light effects, sound, data backup and professional technical advice. We use dynamic lighting effects, gobos, scanners etc to give a real feel of movement, change, and exciting environment. It is associated with warm and soft or alive and kicking, sound. We can certainly come up with ideas for that as well.</p>
        </div>
-    <img class="im" src="" alt="">
+    <img class="im" src="product.jpg" alt="">
 </div>
 <br><br><br>
     <div class="container1">
-    <img class="im" src="" alt="">
+    <img class="im" src="conference.jpg" alt="">
     <div class="container2">
         <h2>CONFERENCE & SEMINARS</h2>
         <p>We ensure success of any conferences or seminars by well crafted event planning & Management also reviewing that’s our strength. Our ability to get into the minutest of the details enables us to secure & handle every aspect right from hospitality to the most advanced technical requirements of any conference or seminar. The ideal customer experience in an event is a comprehensive, yet flexible framework for achieving customer loyalty by learning and understanding your needs.</p>
@@ -114,11 +114,11 @@
         <h2>AWARD NIGHT</h2>
         <p>Having a beautiful backdrop for an award ceremony represents not only the significance of the event but also the respect the organizer has for the attendees, who took time out to attend, and to the award winners for their achievements Our team of designers can transform anything or any place to give you a stylish yet affordable award night.</p>
 </div>
-    <img class="im" src="" alt="">
+    <img class="im" src="award.jpg" alt="">
 </div>
 </div><br><br><br>
 <div class="container1">
-    <img class="im" src="" alt="">
+    <img class="im" src="party.jpg" alt="">
     <div class="container2">
         <h2>THEME PARTY</h2>
         <p>Craftworld themed events are extremely popular and are packed with excitement and guest participation. Page 3 Night Mediterranean Bollywood Jungle James Bond Futuristic Nights Roman Arabian Nights Red Night. Our Craftworld event management company in Delhi can project Logos, messages and create an astonishing launch of your product, surround it with Laser light and present it in an unforgettable way. Read IKFW Review here</p>
@@ -129,9 +129,10 @@
         <h2>SPORTS & ADVENTURES</h2>
         <p>Showcasing of products with professional models elevates the visibility & prominence of any product. To get desired visibility among st the target audience it is of paramount importance. Fashion Shows works very well any event requires that glamour flick be it Customer Dealer Meets, Product Launches We specialise in Fashion Shows by coordinating all the requisite elements. Model Co-ordination (Super models, etc.) Professional choreography Designing Video Shoots for products & Catalogues etc.</p>
        </div>
-    <img class="im" src="" alt="">
+    <img class="im" src="game.jpg" alt="">
 </div>
 </div>
+<h2 class="y"> <a href="contact.php">CONTACT US TO GET STARTED></a></h2>
 
     <br>
     <center><hr class="hr2"></center>
